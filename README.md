fffdea376149ec25b8774058a7a2401d067ac6a7
Oni John <onijohnoluwaseun@gmail.com>
2022-10-28 20:12:17 +0100

    initial project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# HNGi9 Front-End Stage 1 Task.
+# This is my repository for the first stage of the frontend track of HNGi9 intenshhip.
+
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      WELCOME WORLD!
+    </div>
+  )
+}
+
+export default App
