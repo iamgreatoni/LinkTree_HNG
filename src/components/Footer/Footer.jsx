b267ffad9c534308cb82281d7eb95280646bd2e0
Oni John <onijohnoluwaseun@gmail.com>
2022-10-29 00:39:09 +0100

    Web view completed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './Footer.css'
 
+import ZuriIcon from "../../assets/zuri.svg";
+import I4gIcon from "../../assets/I4G.svg";
+
 function Footer() {
   return (
-    <div>
-      Footer Base
-    </div>
+    <footer className="footer">
+      <div className="footer__container">
+        <img src={ZuriIcon} alt="HNG" className="hng__img" />
+        <p className="footer__text">
+          HNG Internship 9 Frontend Task
+        </p>
+        <img src={I4gIcon} alt="Ingressive 4 Good" className="i4g__img" />
+      </div>
+    </footer>
   )
 }
 
